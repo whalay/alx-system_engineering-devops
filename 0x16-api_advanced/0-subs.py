0134e0c1eed69a63200678a6cbd574d29fbad752
root <root@Yusuf.localdomain>
2023-06-06 23:11:07 +0100

    added

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        return data['data']['subscribers']
+    except (requests.HTTPError, KeyError):
+        return 0
